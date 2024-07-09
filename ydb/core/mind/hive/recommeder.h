81ac1fa11b312ed634f722c1e003eac51945003e
Ilia Shakhov <pixcc@ydb.tech>
2024-07-09 08:24:29 +0000

    more updates to experiment

--- ydb/core/mind/hive/recommeder.h
+++ ydb/core/mind/hive/recommeder.h
@@ -11,9 +11,19 @@ struct TRecommenderSettings {
     ui64 ScaleInWindowSize = 0;
 };
 
+enum class ERecommendationDirection {
+    SCALE_OUT,
+    SCALE_IN,
+    SCALE_NOTHING
+};
+
 struct TResourceRecommendation {
     ui64 CpuCores = 0;
     TInstant Timestamp;
+
+    // TODO(pixcc): testing
+    ui64 CurrentCpuCores = 0;
+    ERecommendationDirection Direction = ERecommendationDirection::SCALE_NOTHING;
 };
 
 } // NKikimr::NHive
