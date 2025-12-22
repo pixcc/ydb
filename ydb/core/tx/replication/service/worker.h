40dab4573aaf85ec29725609a93952d795029ac6
Ilia Shakhov <pixcc@ydb.tech>
2025-12-22 10:08:24 +0000

    Add explicit constructor for TEvTerminateWriter

--- ydb/core/tx/replication/service/worker.h
+++ ydb/core/tx/replication/service/worker.h
@@ -92,7 +92,7 @@ struct TEvWorker {
     struct TEvTerminateWriter: public TEventLocal<TEvTerminateWriter, EvTerminateWriter> {
         ui64 PartitionId;
 
-        TEvTerminateWriter(ui64 partitionId);
+        explicit TEvTerminateWriter(ui64 partitionId);
         TString ToString() const override;
     };
 };
